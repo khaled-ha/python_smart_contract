e5b1015fe77c790f01a2a64bf8d24b59330888f1
khaledha <khaledhadjali1@gmail.com>
2022-09-17 15:47:18 +0200

    testing transaction

--- my_first_smart_contract.py
+++ my_first_smart_contract.py
@@ -30,22 +30,32 @@ def get_contract_details():
     with open('compiled_code.json', 'r') as file:
         data = file.read()
         json_data = json.loads(data)
-        print(json_data)
+        get_abi = json.loads(json_data['contracts']['./smart_contract.sol']["SimpleStorage"]["metadata"])["output"]["abi"]
+        # print(get_abi)
+        get_byte_code = json_data["contracts"]["./smart_contract.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+        return get_abi ,get_byte_code
 
 def connect_ganache():
     # set up connection
     w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
     chain_id = 1337
-    my_address = "0x060DFF893981c7Bfa27BB1B0a378fff9af986718"
+    my_address = "0x771A6956987D4F6A7d1E26B078cbEBE783628b8b"
     #private_key = os.getenv("PRIVATE_KEY")
-    private_key = "5549febc40be09772dc85a9e64f77ba7250f368a1a79184227d912f8776d1ac4"
+    private_key = "172375032d0adc123d2c7b3f6ca9eb67ff8825981a87ac85b5cd27facbecf5f8"
     # initialize contract
+    abi, bytecode = get_contract_details()
     SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
     nonce = w3.eth.getTransactionCount(my_address)
     # set up transaction from constructor which executes when firstly
-    transaction = SimpleStorage.constructor().buildTransaction(
-        {"chainId": chain_id, "from": my_address, "nonce": nonce}
-    )
+    transaction = SimpleStorage.constructor().buildTransaction( {
+    "gasPrice": w3.eth.gas_price, 
+    "chainId": chain_id, 
+    "from": my_address, 
+    "nonce": nonce, 
+})
     signed_tx = w3.eth.account.signTransaction(transaction, private_key=private_key)
-    tx_hash = w3.eth.account.send_raw_transaction(signed_tx.rawTransaction)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+if __name__ =='__main__':
+    connect_ganache()
